72631dc1579dafa9028ab205e026d148f3f0429a
NishaRaut2397 <nisharaut2397@gmail.com>
2022-05-05 22:46:00 +0530

    First Commit

---
+++ src/test/java/org/seleniumhq/selenium/MavenJava/SeleniumTest.java
@@ -0,0 +1,37 @@
+package org.seleniumhq.selenium.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void demo1()
+	{
+		System.out.println("Demo1 is a method");
+	}
+	
+	@Test
+	public void demo2()
+	{
+		System.out.println("Demo2 is a method");
+	}
+	
+	@Test
+	public void demo3()
+	{
+		System.out.println("Demo3 is a method");
+	}
+	
+	@Test
+	public void demo4()
+	{
+		System.out.println("Demo4 is a method");
+	}
+	
+	@Test
+	public void demo5()
+	{
+		System.out.println("Demo5 is a method");
+	}
+
+}
